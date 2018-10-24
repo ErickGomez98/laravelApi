47b1460ea518ea1607814c324985abc7ce564522
Erick Gómez <erick0gomez98@gmail.com>
2018-10-24 12:51:06 -0500

    Se creó el factory para el modelo Product
    Signed-off-by: Erick Gómez <erick0gomez98@gmail.com>

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 use Faker\Generator as Faker;
 use App\User;
 use App\Category;
+use App\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,9 @@ use App\Category;
 |
 */
 
+/**
+ * User
+ */
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
@@ -28,9 +32,28 @@ $factory->define(User::class, function (Faker $faker) {
     ];
 });
 
+/**
+ * Category
+ */
 $factory->define(Category::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
         'description' => $faker->paragraph(1)
+    ];
+});
+
+
+/**
+ * Product
+ */
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1, 10),
+        'status' => $faker->randomElement([Product::PRODUCTO_DISPONIBLE, Product::PRODUCTO_NO_DISPONIBLE]),
+        'image' => $faker->randomElement(['1.jpg', '2.jpg', '3.jpg']),
+        //'seller_id' => User::inRandomOrder()->first()->id
+        'seller_id' => User::all()->random()->id
     ];
 });
