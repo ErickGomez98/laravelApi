a2b8cfe227be26e515026eea1029900e8a55c743
Erick Gómez <erick0gomez98@gmail.com>
2018-10-24 12:57:14 -0500

    Se creó el factory para el modelo Transtactions
    Signed-off-by: Erick Gómez <erick0gomez98@gmail.com>

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,7 +4,8 @@ use Faker\Generator as Faker;
 use App\User;
 use App\Category;
 use App\Product;
-
+use App\Transaction;
+use App\Seller;
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -53,7 +54,21 @@ $factory->define(Product::class, function (Faker $faker) {
         'quantity' => $faker->numberBetween(1, 10),
         'status' => $faker->randomElement([Product::PRODUCTO_DISPONIBLE, Product::PRODUCTO_NO_DISPONIBLE]),
         'image' => $faker->randomElement(['1.jpg', '2.jpg', '3.jpg']),
-        //'seller_id' => User::inRandomOrder()->first()->id
         'seller_id' => User::all()->random()->id
+    ];
+});
+
+/**
+ * Transaction
+ */
+$factory->define(Transaction::class, function (Faker $faker) {
+    $vendedor = Seller::has('products')->get()->random();
+    $comprador = User::all()->except($vendedor->id)->random();
+
+    return [
+        'name' => $faker->word,
+        'quantity' => $faker->numberBetween(1, 3),
+        'buyer_id' => $comprador->id,
+        'product_id' => $vendedor->products->random()->id
     ];
 });
